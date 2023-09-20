cf7fe3d217ef4f0eb9da4567913b72dfeab9d381
Oleg Rakov <rakov@cleverstaff.net>
2023-09-20 20:03:13 +0300

    updated css webpack configuration

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import './Counter.scss';
+import classes from './Counter.module.scss';
 
 const Counter = () => {
     const [counter,setCounter] = useState(0);
@@ -9,7 +9,7 @@ const Counter = () => {
     }
 
     return (
-        <div className='Counter'>
+        <div className={classes.Counter}>
             <h1>Counter - {counter}</h1>
             <button onClick={onClickFn}>Click</button>
         </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import {render} from "react-dom";
-import Counter from "./components/Counter";
-// import React from 'react';
+import App from "./App";
 
 render (
     <div>
-        <Counter />
+        <App />
     </div>,
     document.getElementById('root')
 )
